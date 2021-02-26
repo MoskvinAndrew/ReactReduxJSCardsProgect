f9a41dd99c0a173ede7a50cadd7cfdc532262281
Vladislav <vladby86@gmail.com>
2021-02-26 22:22:39 +0300

    was added new styles for navbar, added styles for btns, also was fixed position for links

--- src/n1-main/m1-UI/nav/Button.tsx
+++ src/n1-main/m1-UI/nav/Button.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import './button.module.css';
+import './Button.css';
 
 type ButtonProps = {
     children: any
-    type: string
-    onClick: () => void
-    buttonStyle: string
-    buttonSize: string
+    type?: "button" | "submit" | "reset" | undefined
+    onClick?: () => void
+    buttonStyle?: string
+    buttonSize?: string
 }
 
 
 const STYLES = [
     'btn--primary',
     'btn--outline',
-]
-
+];
 const SIZES = [
     'btn--medium',
     'btn--large',
-]
+    'btn--small',
+];
 
 export const Button: React.FC<ButtonProps> = ({children, buttonSize, buttonStyle, onClick, type}) => {
-    return <>
 
-    </>
+    function predicate(item: string | undefined, arr: string[]) {
+        return arr.find((s: string) => s === item)
+    }
+
+    let btnStyle = (predicate(buttonStyle, STYLES)) ? buttonStyle : STYLES[0];
+    let btnSize = (predicate(buttonSize, SIZES)) ? buttonSize : SIZES[0];
+
+    return <button className={`btn ${btnStyle} ${btnSize}`} type={type} onClick={onClick}>
+        {children}
+    </button>
 }
--- src/n1-main/m1-UI/nav/Nav.tsx
+++ src/n1-main/m1-UI/nav/Nav.tsx
@@ -2,16 +2,27 @@ import React, {useState} from 'react';
 import {RoutingStringConstants} from "../../m3-DAL/routingStringConstants";
 import s from '../nav/nav.module.css';
 import {NavLink} from "react-router-dom";
+import {Button} from "./Button";
 
 export const NavBar = () => {
 
     const navItems = [
-        {title: 'Profile', to: RoutingStringConstants.profile, className: 'nav-link'},
-        {title: 'Packs of cards', to: RoutingStringConstants.packs, className: 'nav-link'},
-        {title: 'Cards', to: RoutingStringConstants.cards, className: 'nav-link'},
-        {title: 'Reset Password', to: RoutingStringConstants.passwordRecovery, className: 'nav-link'},
-        {title: 'New Password', to: RoutingStringConstants.newPassword, className: 'nav-link'},
-        {title: 'test page', to: RoutingStringConstants.testPage, className: 'nav-link'},
+        {title: 'Profile', to: RoutingStringConstants.profile, className: 'nav-link', classNameLi: 'nav__menu_li'},
+        {title: 'Packs of cards', to: RoutingStringConstants.packs, className: 'nav-link', classNameLi: 'nav__menu_li'},
+        {title: 'Cards', to: RoutingStringConstants.cards, className: 'nav-link', classNameLi: 'nav__menu_li'},
+        {
+            title: 'Reset Password',
+            to: RoutingStringConstants.passwordRecovery,
+            className: 'nav-link',
+            classNameLi: 'nav__menu_li'
+        },
+        {
+            title: 'New Password',
+            to: RoutingStringConstants.newPassword,
+            className: 'nav-link',
+            classNameLi: 'nav__menu_li'
+        },
+        {title: 'test page', to: RoutingStringConstants.testPage, className: 'nav-link', classNameLi: 'nav__menu_li'},
     ];
 
     const [isActiveMode, setIsActiveMode] = useState<boolean>(false)
@@ -25,13 +36,13 @@ export const NavBar = () => {
             <NavLink to={RoutingStringConstants.profile} className={s.navBar__block_logo}>
                 <h1>Logo</h1>
             </NavLink>
-            <div className={'menu-icon'} onClick={onClickHandler}>
+            <div className={s['menu-icon']} onClick={onClickHandler}>
                 <i className={isActiveMode ? 'fas fa-times' : 'fas fa-bars'}></i>
             </div>
             <ul className={isActiveMode ? `${s.nav_menu} ${s.active}` : `${s.nav_menu}`}>
                 {navItems.map((item, index) => {
                     return (
-                        <li key={index}>
+                        <li key={index} className={s[`${item.classNameLi}`]}>
                             <NavLink to={item.to} className={s[`${item.className}`]}>
                                 {item.title}
                             </NavLink>
@@ -41,10 +52,14 @@ export const NavBar = () => {
             </ul>
             <div className={s.navBar__block_btn}>
                 <NavLink to={RoutingStringConstants.login}>
-                    Sign in
+                    <Button buttonSize={"btn--small"} buttonStyle={"btn--outline"}>
+                        Sign In
+                    </Button>
                 </NavLink>
                 <NavLink to={RoutingStringConstants.registration}>
-                    Sign up
+                    <Button buttonStyle={'btn--outline'} buttonSize={'btn--small'}>
+                        Sign Up
+                    </Button>
                 </NavLink>
             </div>
         </nav>
